2a9100addd52b34e68c555154ddbcc1010600d64
ChristofferBillman <christoffer.billman@gmail.com>
2021-12-17 01:41:24 +0100

    Implemented "StudpidPathfinder"
    It just marks squares visited, such that it (kind of)
    does the DVD-bouncy thing.

--- src/scripts/StupidPathfinder.ts
+++ src/scripts/StupidPathfinder.ts
@@ -7,17 +7,33 @@ export default class StupidPathfinder{
 	arr: Square.Square[];
 	squares: Square.Square[][];
 
+	dx: number = 1;
+	dy: number = 1;
+
 	constructor(world: Square.Square[][]){
-		this.pos = new ValuePair(0,0)
+		this.pos = new ValuePair(2,0)
 		this.arr = [];
 		this.squares = world;
 
-		setInterval(this.iterate,500,this);
+		setInterval(this.iterate,250,this);
 	}
-	public iterate(instance: this){
+	private iterate(t: this){
+		// t is short for "this".
+		if(t.pos.x >= t.squares.length-1){
+			t.dx = -1;
+		}
+		if(t.pos.x <= 0){
+			t.dx = 1;
+		}
+		if(t.pos.y >= t.squares[0].length-1){
+			t.dy = -1;
+		}
+		if(t.pos.y <= 0){
+			t.dy = 1;
+		}
+		t.squares[t.pos.x][t.pos.y].visited = true;
 
-		console.log(instance.pos)
-		//pos.y++;
-		//squares[pos.x][pos.y].visited = true;
+		t.pos.y = t.pos.y + t.dy;
+		t.pos.x = t.pos.x + t.dx;
 	}
 }
