39d2ce939777f3eb63bd097e1018acc7fead67e7
ChristofferBillman <christoffer.billman@gmail.com>
2021-12-18 16:30:01 +0100

    Added module grid.
    Contains all logic regarding the grid.

---
+++ src/scripts/Grid.ts
@@ -0,0 +1,57 @@
+import Square from './Square'
+import UI from './UI';
+import ValuePair from './ValuePair'
+
+module Grid {
+	let grid: Square.Square[][];
+	let squareSize: number = 50;
+	let gap: number = 5;
+
+	export function init(width: number, height: number): Square.Square[][]{
+		makeGrid(width, height,squareSize,gap)
+		return grid;
+	}
+
+	export function onframe(){
+		for(let i = 0; i < grid.length; i++){
+			for(let j = 0; j < grid[i].length; j++){
+				grid[i][j].onframe((UI.UIObjects.get('eraseButton') as UI.Button)!.pressed);
+			}
+		}
+	}
+	export function makeGrid(width: number, height: number, squareSize: number, gap: number){
+		let i: number = 0;
+		let j: number = 0;
+
+		grid = [];
+
+		for(let x = 0; x < width; x = x + squareSize+gap){
+			grid[i] = [];
+			for(let y = 0; y < height; y = y + squareSize+gap){
+				grid[i][j] = new Square.Square(squareSize, new ValuePair(x,y));
+				j++;
+			}
+			j = 0;
+			i++;
+		}
+	}
+	export function isInGrid(pos: ValuePair){
+		return pos.x < grid.length-1 && pos.x > 0 && pos.y < grid[0].length-1 && pos.y > 0;
+	}
+	export function unsetGoals(){
+		for(let i = 0; i < grid.length; i++){
+			for(let j = 0; j < grid[i].length; j++){
+				grid[i][j].unsetGoal();
+			}
+		}
+	}
+	export function unsetStarts(){
+		for(let i = 0; i < grid.length; i++){
+			for(let j = 0; j < grid[i].length; j++){
+				grid[i][j].unsetStart();
+			}
+		}
+	}
+}
+
+export default Grid;
